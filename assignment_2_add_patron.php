aab9e730e1a533cbad6d93eac998317a488eeb1a
delanyburk-sjsu <delany.burk@sjsu.edu>
2026-08-26 18:08:22 -0700

    Update assignment_2_add_patron.php

--- assignment_2_add_patron.php
+++ assignment_2_add_patron.php
@@ -1,1 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>303 HTML Forms</title>
+	<link rel="stylesheet" type="text/css" href="css/basic.css" />
+</head>
 
+<body>
+
+<h3>Thank You For Registering!</h3>
+
+<?php
+	$name = $_POST['fname''lname'];
+	print "<p>Name: $name</p>";
+  $email = $_POST['email'];
+  print "<p>Email: $email</p>
+  $cities = $_POST['cities'];
+  print "<p>city: $cities</p>
+?>
+
+<p>You've just written your first PHP program!</p>
+
+</body>
+</html>
